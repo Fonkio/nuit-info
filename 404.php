2c29d5397f462097dd8a82fa51bfa303022d7a84
¨Maxime F <maxime.fabre10@gmail.com>
2018-12-07 02:30:15 +0100

    nioque tes morts

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 ERROR</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>ERREUR 404 : Gagne ce labyrinthe pour retourner sur le site</h1>
+<?php 
+	
+ ?>
+<center><script src="game.js"></script></center>
+</div>
+</body>
+</html>
